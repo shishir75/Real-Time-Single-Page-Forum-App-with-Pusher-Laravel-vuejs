b0b8c57644ce7f8420456dbd5687a9e2f02ee785
shishir75 <shishir.srdr16@gmail.com>
2020-06-28 20:18:39 +0600

    Factories

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,11 +2,22 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Category;
 use App\Model\Question;
+use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentence,
+        'slug' => Str::slug($faker->sentence),
+        'body' => $faker->text,
+        'category_id' => function() {
+            return Category::all()->random();
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
