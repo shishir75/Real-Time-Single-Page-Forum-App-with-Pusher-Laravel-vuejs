ae25283efc467c8af68b10e2f6d034cee3f093e5
shishir75 <shishir.srdr16@gmail.com>
2020-07-03 02:19:02 +0600

    Home Component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-    <title>Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Single Page Real Time Forum</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 <div id="app">
     <v-app>
-        <v-toolbar>
-            <v-toolbar-side-icon></v-toolbar-side-icon>
-            <v-toolbar-title>Title</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn flat>Link One</v-btn>
-                <v-btn flat>Link Two</v-btn>
-                <v-btn flat>Link Three</v-btn>
-            </v-toolbar-items>
-        </v-toolbar>
+        <app-home></app-home>
     </v-app>
 </div>
 
