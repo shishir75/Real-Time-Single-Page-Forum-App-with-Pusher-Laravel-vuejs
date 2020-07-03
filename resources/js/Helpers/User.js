b0dd69272cdfcd75220bfe289308a60eb07782fe
shishir75 <shishir.srdr16@gmail.com>
2020-07-04 00:00:25 +0600

    Login & Logout Helpers

--- resources/js/Helpers/User.js
+++ resources/js/Helpers/User.js
@@ -15,6 +15,37 @@ class User {
             AppStorage.store(username, access_token);
         }
     }
+
+    hasToken() {
+        const storedToken = AppStorage.getToken();
+
+        if (storedToken) {
+            return Token.isValid(storedToken) ? true : false;
+        }
+        return false;
+    }
+
+    loggedIn() {
+        return this.hasToken();
+    }
+
+    logout() {
+        AppStorage.clear();
+    }
+
+    name() {
+        if(this.loggedIn()) {
+            return AppStorage.getUser();
+        }
+    }
+
+    id() {
+        if(this.loggedIn()) {
+            const payload = Token.payload(AppStorage.getToken());
+            return payload.sub;
+        }
+    }
+
 }
 
 export default User = new User();
