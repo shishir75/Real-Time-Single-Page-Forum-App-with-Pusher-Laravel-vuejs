344c0b67c48e42671b72d2a2edb3ac4312109079
shishir75 <shishir.srdr16@gmail.com>
2020-07-12 20:15:52 +0600

    Fetch all Questions

--- resources/js/components/Forum/Forum.vue
+++ resources/js/components/Forum/Forum.vue
@@ -2,7 +2,7 @@
     <v-container fluid grid-list-md>
         <v-layout row wrap>
             <v-flex xs8>
-                <question></question>
+                <question v-for="question in questions" :key="question.path" :data="question"></question>
             </v-flex>
             Sidebar
         </v-layout>
@@ -13,8 +13,18 @@
     import Question from "./Question";
     export default {
         name: "Forum",
+        data() {
+          return {
+              questions: {}
+          }
+        },
         components: {
             question: Question,
+        },
+        created() {
+            axios.get('/api/question')
+                .then(res => this.questions = res.data.data)
+                .catch(error => console.log(error.response.data));
         }
     }
 </script>
--- resources/js/components/Forum/Question.vue
+++ resources/js/components/Forum/Question.vue
@@ -2,19 +2,24 @@
     <v-card>
         <v-card-title primary-title>
             <div>
-                <h3 class="headline mb-0">Title of Question</h3>
-                <div class="grey--text">Said 4 Hours Ago</div>
+                <h3 class="headline mb-0">
+                    <router-link :to="data.path">
+                        {{ data.title }}
+                    </router-link>
+                </h3>
+                <div class="grey--text">{{ data.created_at }}</div>
             </div>
         </v-card-title>
         <v-card-text>
-            Text
+            {{ data.body }}
         </v-card-text>
     </v-card>
 </template>
 
 <script>
     export default {
-        name: "Question"
+        name: "Question",
+        props: ['data'],
     }
 </script>
 
