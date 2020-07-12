8cf5258112405d3b08c0ede5e846003c8241e4f0
shishir75 <shishir.srdr16@gmail.com>
2020-07-12 19:52:43 +0600

    Forum Component

--- resources/js/components/Forum/Forum.vue
+++ resources/js/components/Forum/Forum.vue
@@ -1,10 +1,21 @@
 <template>
-    <h1>Forum</h1>
+    <v-container fluid grid-list-md>
+        <v-layout row wrap>
+            <v-flex xs8>
+                <question></question>
+            </v-flex>
+            Sidebar
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
+    import Question from "./Question";
     export default {
-        name: "Forum"
+        name: "Forum",
+        components: {
+            question: Question,
+        }
     }
 </script>
 
---
+++ resources/js/components/Forum/Question.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-card>
+        <v-card-title primary-title>
+            <div>
+                <h3 class="headline mb-0">Title of Question</h3>
+                <div class="grey--text">Said 4 Hours Ago</div>
+            </div>
+        </v-card-title>
+        <v-card-text>
+            Text
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "Question"
+    }
+</script>
+
+<style scoped>
+
+</style>
