7ec2ada9b9e7ca0dff03054acad728b560d19c57
shishir75 <shishir.srdr16@gmail.com>
2020-07-14 20:10:25 +0600

    Category Store

--- resources/js/Router/router.js
+++ resources/js/Router/router.js
@@ -9,13 +9,14 @@ import Forum from "../components/Forum/Forum";
 import Logout from "../components/Login/Logout";
 import Read from "../components/Forum/Read";
 import Create from "../components/Forum/Create";
-
+import CreateCategory from "../components/Category/CreateCategory";
 
 const routes = [
     { path: '/login', name: 'login', component: Login },
     { path: '/logout', name: 'logout', component: Logout },
     { path: '/signup',name: 'signup', component: Signup },
     { path: '/forum',name: 'forum', component: Forum },
+    { path: '/category',name: 'category', component: CreateCategory },
     { path: '/ask',name: 'Create', component: Create },
     { path: '/question/:slug',name: 'read', component: Read },
 ];
---
+++ resources/js/components/Category/CreateCategory.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container>
+        <v-form @submit.prevent="create">
+            <v-text-field
+                v-model="form.name"
+                label="Category Name"
+                type="text"
+                required
+            ></v-text-field>
+
+            <v-btn type="submit" color="teal">Create</v-btn>
+        </v-form>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "CreateCategory",
+        data() {
+            return {
+                form: {
+                    name: null,
+                }
+            }
+        },
+        methods: {
+            create() {
+                axios.post('/api/category', this.form)
+                    .then(res => console.log(res.data.data))
+                    .catch(error => console.log(error.response.data));
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
