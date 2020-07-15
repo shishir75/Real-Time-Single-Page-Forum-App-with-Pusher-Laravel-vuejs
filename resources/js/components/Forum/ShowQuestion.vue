6c46dad9fcd35d9793db6630acccc4c9cb2281da
shishir75 <shishir.srdr16@gmail.com>
2020-07-15 15:13:24 +0600

    Reply Count instant update after new reply created

--- resources/js/components/Forum/ShowQuestion.vue
+++ resources/js/components/Forum/ShowQuestion.vue
@@ -9,7 +9,7 @@
                     <span class="grey--text">{{ data.user }} said {{ data.created_at }}</span>
                 </div>
                 <v-spacer></v-spacer>
-                <v-btn color="teal" dark>{{ data.reply_count }} Replies</v-btn>
+                <v-btn color="teal" dark>{{ this.reply_count }} Replies</v-btn>
             </v-card-title>
             <v-card-text v-html="body"></v-card-text>
 
@@ -33,12 +33,16 @@
         data() {
             return {
                 own : User.own(this.data.user_id),
+                reply_count: this.data.reply_count,
             }
         },
         computed: {
             body() {
                 return md.parse(this.data.body);
             }
+        },
+        created() {
+            this.listen();
         },
         methods: {
             destroy() {
@@ -50,6 +54,11 @@
             },
             edit() {
                 EventBus.$emit('startEditing');
+            },
+            listen() {
+                EventBus.$on('newReply', () => {
+                    this.reply_count++;
+                })
             }
         }
     }
